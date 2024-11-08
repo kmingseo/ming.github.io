b17e9aaa309e60bb472dde302288c611dae3b5ab
kmingseo <ms7happy@naver.com>
2024-11-08 16:55:06 +0900

    update config

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731050267',resources: [
+    cacheName: 'chirpy-1731052468',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
